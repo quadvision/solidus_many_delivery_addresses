f5e9d3667b1aacb1d6d7eee471f5617fa7179bdb
Berlimioz <berlimioz@gmail.com>
2016-11-04 10:55:15 +0100

    add validation on quantity presence when new address

--- app/models/spree/line_item_address.rb
+++ app/models/spree/line_item_address.rb
@@ -8,12 +8,21 @@ module Spree
 
     validates :address, presence: true
 
+    before_create :ensure_quantity_for_new_address
+
     def amount
       self.line_item.price * quantity
     end
 
     extend Spree::DisplayMoney
     money_methods :amount
+
+    private
+    def ensure_quantity_for_new_address
+      if address.new_record?
+        errors.add(:quantity, I18n.t('activerecord.errors.models.spree/line_item.attributes.quantity.blank_for_new_address')) unless quantity && quantity > 0
+      end
+    end
 
   end
 end
